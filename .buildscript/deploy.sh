6e4891bd9f1f71b74bf276974004f6ad68287352
Wenhui Yao <wenhuiyao01@gmail.com>
2017-11-21 12:50:35 -0800

    Cleanup gradle file

--- .buildscript/deploy.sh
+++ .buildscript/deploy.sh
@@ -3,12 +3,8 @@
 JDK="oraclejdk8"
 BRANCH="master"
 
-set -e
-
 if [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
     echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-    echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
     echo "Deploying snapshot..."
     ./gradlew build bintrayUpload
